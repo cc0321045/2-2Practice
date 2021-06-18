5a4897aab30a8e632f23cb6d8ec950c3988c4695
cc0321045 <adam0012367@gmail.com>
2021-06-18 17:56:44 +0800

    閏年

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -5,10 +5,21 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace _2_2Practice {
-    public partial class Default : System.Web.UI.Page {
-
-        protected void Page_Load(object sender, EventArgs e) {
+namespace _2_2Practice
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_N = 700;
+            if ((i_N % 4) == 0 || (i_N % 400) == 0 && (i_N % 100) != 0)
+            {
+                Response.Write("Y");
+            }
+            else
+            {
+                Response.Write("N");
+            }
         }
     }
 }
